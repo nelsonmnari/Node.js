d913997619b328f01b3950391b93c4a99f05686e
nelsonmnari <mnarinelson10@gmail.com>
2025-06-13 01:43:35 +0300

    createFilesWithNodejs

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) =>
 
 console.log('Hello ....');
 
+fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you.',  (err,) => {
+    if (err) throw err;
+    console.log('write complete');
+
+})
+
+fs.appendFile(path.join(__dirname, 'files', 'test.txt'), 'Testing text.', (err) => {
+    if (err) throw err;
+    console.log('Append complete');
+
+})
+
+
 // Exit on uncaught errors
 
 process.on('uncaughtException', err => {
